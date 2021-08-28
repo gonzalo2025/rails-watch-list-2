9cc89d3fdd76c1fb6ac94fbd12a5854b33e463d3
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <grupodigitalgrafico@gmail.com>
2021-08-28 19:30:57 -0400

    watch list 2 update

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,7 +5,7 @@ class BookmarksController < ApplicationController
     def new
       @bookmark = Bookmark.new
     end
-  
+      
     def create
       @bookmark = Bookmark.new(bookmark_params)
       @bookmark.list = @list
@@ -17,19 +17,18 @@ class BookmarksController < ApplicationController
       @bookmark.destroy
       redirect_to list_path(@bookmark.list)
     end
-  
+      
     private
-  
+      
     def bookmark_params
       params.require(:bookmark).permit(:comment, :movie_id)
     end
-  
+      
     def set_bookmark
       @bookmark = Bookmark.find(params[:id])
     end
-  
+      
     def set_list
       @list = List.find(params[:list_id])
     end
 end
-  
